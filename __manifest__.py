2e59a4fbf3c0c23c6fbda2baf02f83ee922ffac7
Champ <kkw.phattarapon@gmail.com>
2020-01-14 13:51:01 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Partner Member Type",
+    'description': 'Add Member Type on partner',
+    "category": "Extra Tools",
+    "summary": "Add Member Type on partner",
+    'version': '12.0.1.0.0',
+    "author": "CHAMP",
+    "depends": ["base"],
+    "data": ["views/res_partner.xml"],
+    "installable": True,
+    'application': False,
+}
